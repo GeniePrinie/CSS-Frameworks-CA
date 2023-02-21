2e16a8c49bc8aea46fcad71c315b66023b21d409
GeniePrinie <geniexpn@gmail.com>
2023-02-21 13:05:14 +0100

    made the registerform js works

--- js/handlers/registerForm.mjs
+++ js/handlers/registerForm.mjs
@@ -1,14 +1,18 @@
-export function setRegisterFormListener() {
-  const formNewAccount = document.querySelector(".form-new-account");
+import { register } from "../API/auth/register.mjs";
 
+export function setRegisterFormListener() {
+  const formNewAccount = document.querySelector(".form-register");
+  console.log("Before event listener - making sure function runs");
   formNewAccount.addEventListener("submit", (e) => {
-    if (!formNewAccount.checkValidity()) {
-      e.preventDefault();
-      const form = e.target;
-      const formData = new FormData(form);
-      const profile = Object.fromEntries(formData.entries());
-      console.log("yes");
-    }
+    formNewAccount.checkValidity();
     formNewAccount.classList.add("was-validated");
+
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const profile = Object.fromEntries(formData.entries());
+
+    register(profile);
   });
 }
+//Password for gg (gg@stud.noroff.no) is ggTestPassword
